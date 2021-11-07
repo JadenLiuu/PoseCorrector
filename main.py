e8173d78768c6901c049135d6bc3360e933c0517
jadenliuuu <r35191007@gmail.com>
2021-11-07 21:52:05 +0800

    feat: approximate eye loc

--- main.py
+++ main.py
@@ -4,7 +4,12 @@ import argparse
 
 args = argparse.ArgumentParser()
 args.add_argument('-p', '--path', type=str, default='./tests/videos/test4.mp4')
+args.add_argument('--eye', type=float, nargs='+', default=[0.24, 0.2, 0.2, 0.2])
 opt = args.parse_args()
 
 if __name__ == '__main__':
-    Detector.start(opt.path)
+    if len(opt.eye) > 4:
+        print("opt.eye must be len <= 4")
+    else:
+        Detector.setConfig(*opt.eye)
+        Detector.start(opt.path)
