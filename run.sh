8e105376e317c29e48e8806ecec2b14baf72cd9d
jadenliuuu <r35191007@gmail.com>
2021-11-27 19:27:59 +0800

    feat(ai): change folder

--- run.sh
+++ run.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-sudo docker run --rm --gpus all -v `pwd`:/work/ -i -t nvcr.io/pytorch/gundev:latest bash
+sudo docker run --rm --gpus all \
+                 -v `pwd`:/work/ \
+                 -p 8000:8000 \
+                 --network host \
+                 -i -t nvcr.io/pytorch/gundev:v_2.0 bash
